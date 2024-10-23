8c7ffc31acb227247e831d216d5fb2a1d685f2ac
Jacob Jiskoot <jajiskoot@yahoo.com>
2024-10-23 19:22:19 -0400

    Updates

---
+++ static/js/416.749c9f47.chunk.js
@@ -0,0 +1,18 @@
+"use strict";(self.webpackChunkdeveloper_site=self.webpackChunkdeveloper_site||[]).push([[416],{9416:(e,n,i)=>{i.r(n),i.d(n,{default:()=>x});var t=i(1319),d=i(4647),r=i(9281),s=i(4019),l=i(5639),c=i(9);const a=(0,c.Ay)("section")`
+  position: relative;
+  padding: 6.5rem 0 3rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 1024px) {
+    padding: 5.5rem 0 3rem;
+  }
+`,o=(0,c.Ay)("p")`
+  padding: 0.75rem 0 0.75rem;
+`,m=(0,c.Ay)("div")`
+  @media only screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`;var h=i(579);const x=(0,r.C)()((e=>{let{title:n,content:i,button:r,t:c}=e;return(0,h.jsx)(a,{children:(0,h.jsx)(s.q7,{direction:"up",children:(0,h.jsx)(t.A,{justify:"center",align:"middle",children:(0,h.jsx)(m,{children:(0,h.jsxs)(d.A,{lg:24,md:24,sm:24,xs:24,children:[(0,h.jsx)("h6",{children:c(n)}),(0,h.jsx)(o,{children:c(i)}),r&&(0,h.jsx)(l.$,{name:"submit",onClick:()=>{return e="mission",void document.getElementById(e).scrollIntoView({behavior:"smooth"});var e},children:c(r)})]})})})})})}))}}]);
+//# sourceMappingURL=416.749c9f47.chunk.js.map
